1a45df5043cb07eecb72f730685da22035705e4d
Yasuhiro Nishihara <yasuhiro1026a@hotmail.com>
2018-11-07 00:04:34 +0900

    add side bar item.

--- src/components/parts/SideBar.vue
+++ src/components/parts/SideBar.vue
@@ -1,16 +1,47 @@
 <template>
   <div>
     <slide right noOverlay>
+      <side-bar-item v-for="(item, index) in list" :key="index" :main="item.main" :sub="item.sub">
+      </side-bar-item>
     </slide>
   </div>
 </template>
 
 <script>
 import {Slide} from 'vue-burger-menu';
+import SideBarItem from '@/components/parts/SideBarItem';
+
+const itemList = [
+  {
+    main: 'home',
+    sub: '',
+  },
+  {
+    main: 'games',
+    sub: 'play games',
+  },
+  {
+    main: 'company',
+    sub: 'about us',
+  },
+  {
+    main: 'contact',
+    sub: 'send a mail',
+  },
+];
 
 export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  mounted() {
+    this.list = itemList;
+  },
   components: {
     Slide,
+    SideBarItem,
   },
 };
 </script>
---
+++ src/components/parts/SideBarItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="main-text">
+      {{main}}
+    </div>
+    <div class="sub-text">
+      {{sub}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    main: {
+      type: String,
+      require: true,
+      default: '',
+    },
+    sub: {
+      type: String,
+      require: false,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  height: 70px;
+}
+.main-text {
+  font-size: 14px;
+}
+.sub-text {
+  font-size: 10px;
+}
+</style>
